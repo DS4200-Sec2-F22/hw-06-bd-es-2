ff4bbc391bd3095e6c8574064750423e7b8ea8ce
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-10-28 10:20:12 -0400

    finished

--- js/main.js
+++ js/main.js
@@ -14,51 +14,13 @@ const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
 
 // set up frame 1
 const FRAME1 = d3.select("#column1")
-  .append("svg")
-  .attr("height", FRAME_HEIGHT)
-  .attr("width", FRAME_WIDTH)
-  .attr("class", "frame");
+.append("svg")
+.attr("height", FRAME_HEIGHT)
+.attr("width", FRAME_WIDTH)
+.attr("class", "frame");
 
 // access the data from the csv file
 d3.csv("data/iris.csv").then((data) => {
-<<<<<<< HEAD
-
-  const MAX_SEPAL_LENGTH = d3.max(data, (d) => {
-    return parseFloat(d.Sepal_Length);
-  });
-
-
-  const MAX_PETAL_LENGTH = d3.max(data, (d) => {
-    return parseFloat(d.Petal_Length);
-  });
-
-  const MAX_SEPAL_WIDTH = d3.max(data, (d) => {
-    return parseFloat(d.Sepal_Width);
-  });
-
-
-  const MAX_PETAL_WIDTH = d3.max(data, (d) => {
-    return parseFloat(d.Petal_Width);
-  });
-
-
-  const SEPAL_LENGTH_SCALE = d3.scaleLinear()
-    .domain([0, MAX_SEPAL_LENGTH])
-    .range([0, VIS_WIDTH])
-
-  const PETAL_LENGTH_SCALE = d3.scaleLinear()
-    .domain([0, MAX_PETAL_LENGTH])
-    .range([VIS_HEIGHT, 0])
-
-  const SEPAL_WIDTH_SCALE = d3.scaleLinear()
-    .domain([0, MAX_SEPAL_WIDTH])
-    .range([0, VIS_WIDTH])
-
-  const PETAL_WIDTH_SCALE = d3.scaleLinear()
-    .domain([0, MAX_PETAL_WIDTH])
-    .range([VIS_HEIGHT, 0])
-
-=======
   
   // find max lengths and widths
   const MAX_SEPAL_LENGTH = d3.max(data, (d) => {
@@ -94,43 +56,15 @@ d3.csv("data/iris.csv").then((data) => {
   .domain([0, MAX_PETAL_WIDTH])
   .range([VIS_HEIGHT, 0]);
   
->>>>>>> 1302d8fec16b7947576c59b12b5c273fcca016f0
   // add x axis to the first graph
   FRAME1.append("g")
-    .attr("transform", "translate(" + PADDING + ","
-      + (VIS_HEIGHT + MARGINS.top) + ")")
-    .call(d3.axisBottom(SEPAL_LENGTH_SCALE).ticks(8))
-    .attr("font-size", "10px");
-
+  .attr("transform", "translate(" + PADDING + ","
+  + (VIS_HEIGHT + MARGINS.top) + ")")
+  .call(d3.axisBottom(SEPAL_LENGTH_SCALE).ticks(8))
+  .attr("font-size", "10px");
+  
   // add y axis to the  first graph
   FRAME1.append("g")
-<<<<<<< HEAD
-    .attr("transform", "translate(" + PADDING + ","
-      + MARGINS.top + ")")
-    .call(d3.axisLeft(PETAL_LENGTH_SCALE).ticks(15))
-    .attr("font-size", "10px");
-
-
-  FRAME1.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", (d) => { return SEPAL_LENGTH_SCALE(d.Sepal_Length) + PADDING; })
-    .attr("cy", (d) => { return PETAL_LENGTH_SCALE(d.Petal_Length) + PADDING; })
-    .attr("r", 2)
-    .attr("class", "point")
-    .attr("id", (d) => { return d.id })
-    .style("fill", function (d) {
-      if (d.Species == "virginica") {
-        return "LightSkyBlue";
-      } else if (d.Species == "versicolor") {
-        return "LightSalmon";
-      } else {
-        return "LightGreen";
-      }
-    })
-
-=======
   .attr("transform", "translate(" + PADDING + ","
   + MARGINS.top + ")")
   .call(d3.axisLeft(PETAL_LENGTH_SCALE).ticks(15))
@@ -141,11 +75,11 @@ d3.csv("data/iris.csv").then((data) => {
   .data(data)
   .enter()
   .append("circle")
-  .attr("cx", (d) => { return SEPAL_LENGTH_SCALE(d.Sepal_Length) + PADDING ;})
-  .attr("cy", (d) => { return PETAL_LENGTH_SCALE(d.Petal_Length) + PADDING ;})
+  .attr("cx", (d) => { return SEPAL_LENGTH_SCALE(d.Sepal_Length) + PADDING; })
+  .attr("cy", (d) => { return PETAL_LENGTH_SCALE(d.Petal_Length) + PADDING; })
   .attr("r", r)
   .attr("class", "point")
-  .attr("id", (d) => {return d.id;})
+  .attr("id", (d) => { return d.id; })
   .style("fill", function (d) {
     if (d.Species == "virginica") {
       return "LightSkyBlue";
@@ -157,130 +91,21 @@ d3.csv("data/iris.csv").then((data) => {
   })
   
   // create second frame
->>>>>>> 1302d8fec16b7947576c59b12b5c273fcca016f0
   const FRAME2 = d3.select("#column2")
-    .append("svg")
-    .attr("height", FRAME_HEIGHT)
-    .attr("width", FRAME_WIDTH)
-    .attr("class", "frame");
-
+  .append("svg")
+  .attr("height", FRAME_HEIGHT)
+  .attr("width", FRAME_WIDTH)
+  .attr("class", "frame");
+  
   // add x axis to the second graph
   FRAME2.append("g")
-    .attr("transform", "translate(" + PADDING + ","
-      + (VIS_HEIGHT + MARGINS.top) + ")")
-    .call(d3.axisBottom(SEPAL_WIDTH_SCALE).ticks(7))
-    .attr("font-size", "10px");
-
+  .attr("transform", "translate(" + PADDING + ","
+  + (VIS_HEIGHT + MARGINS.top) + ")")
+  .call(d3.axisBottom(SEPAL_WIDTH_SCALE).ticks(7))
+  .attr("font-size", "10px");
+  
   // add y axis to the second graph
   FRAME2.append("g")
-<<<<<<< HEAD
-    .attr("transform", "translate(" + PADDING + ","
-      + MARGINS.top + ")")
-    .call(d3.axisLeft(PETAL_WIDTH_SCALE).ticks(8))
-    .attr("font-size", "10px");
-
-  const points = FRAME2.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", (d) => { return SEPAL_WIDTH_SCALE(d.Sepal_Width) + PADDING; })
-    .attr("cy", (d) => { return PETAL_WIDTH_SCALE(d.Petal_Width) + PADDING; })
-    .attr("r", 2)
-    .attr("class", "point")
-    .attr("id", (d) => { return d.id })
-    .style("fill", function (d) {
-      if (d.Species == "virginica") {
-        return "LightSkyBlue";
-      } else if (d.Species == "versicolor") {
-        return "LightSalmon";
-      } else {
-        return "LightGreen";
-      }
-    })
-
-  const selectedPoints = [];
-
-  FRAME2
-    .call(d3.brush()
-      .extent([[PADDING, PADDING], [FRAME_WIDTH, FRAME_HEIGHT - PADDING]])
-      .on("start brush", updateChart))
-    .on("end brush", changePoints)
-
-  function updateChart(event) {
-    console.log(event)
-    extent = event.selection
-    points.classed("selected", function (d) { return isBrushed(extent, SEPAL_LENGTH_SCALE(d.Sepal_Length), PETAL_LENGTH_SCALE(d.Petal_Length)) },
-      selectedPoints.push(event.selection.id)
-    )
-  }
-
-  function isBrushed(brush_coords, cx, cy) {
-    var x0 = brush_coords[0][0],
-      x1 = brush_coords[1][0],
-      y0 = brush_coords[0][1],
-      y1 = brush_coords[1][1];
-    return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;    // This return TRUE or FALSE depending on if the points is in the selected area
-  }
-
-  function changePoints() {
-    for (let i = 0; i < length(selectedPoints); i++) {
-      for (let j = 0; j < length(selectedPoints); j++) {
-        FRAME1.selectAll("#" + j)
-          .enter()
-          .attr("class", "selection")
-      }
-    }
-  }
-
-  const FRAME3 = d3.select("#column3")
-    .append("svg")
-    .attr("height", FRAME_HEIGHT)
-    .attr("width", FRAME_WIDTH)
-    .attr("class", "frame");
-
-  const FRAME3_X_AXIS = ["Setosa", "Versicolor", "Virginica"]
-
-  const X_AXIS = d3.scaleBand()
-    .range([0, VIS_WIDTH])
-    .domain(FRAME3_X_AXIS.map(function (d) { return d }))
-    .padding(0.5);
-
-
-  FRAME3.append("g")
-    .attr("transform", "translate(" + PADDING + ","
-      + (VIS_HEIGHT + MARGINS.top) + ")")
-    .call(d3.axisBottom(X_AXIS))
-
-  const Y_AXIS = d3.scaleLinear()
-    .domain([0, 50])
-    .range([VIS_HEIGHT, 0])
-
-  // add y axis to the second graph
-  FRAME3.append("g")
-    .attr("transform", "translate(" + PADDING + ","
-      + MARGINS.top + ")")
-    .call(d3.axisLeft(Y_AXIS).ticks(12))
-    .attr("font-size", "8px");
-
-
-
-  // Bars
-  const CATEGORIES = { 0: "setosa", 1: "versicolor", 2: "virginica" }
-
-
-  for (let i = 0; i < 3; i++) {
-    FRAME3.append("rect")
-      .attr("x", ((i + 1) * 100) - 12.5)
-      .attr("y", 50)
-      .attr("width", 75)
-      .attr("height", VIS_HEIGHT)
-      .attr("class", CATEGORIES[i])
-  }
-
-})
-
-
-=======
   .attr("transform", "translate(" + PADDING + ","
   + MARGINS.top + ")")
   .call(d3.axisLeft(PETAL_WIDTH_SCALE).ticks(8))
@@ -291,11 +116,11 @@ d3.csv("data/iris.csv").then((data) => {
   .data(data)
   .enter()
   .append("circle")
-  .attr("cx", (d) => { return SEPAL_WIDTH_SCALE(d.Sepal_Width) + PADDING ;})
-  .attr("cy", (d) => { return PETAL_WIDTH_SCALE(d.Petal_Width) + PADDING ;})
+  .attr("cx", (d) => { return SEPAL_WIDTH_SCALE(d.Sepal_Width) + PADDING; })
+  .attr("cy", (d) => { return PETAL_WIDTH_SCALE(d.Petal_Width) + PADDING; })
   .attr("r", r)
   .attr("class", "point")
-  .attr("id", (d) => { return d.id ;})
+  .attr("id", (d) => { return d.id; })
   .style("fill", function (d) {
     if (d.Species == "virginica") {
       return "LightSkyBlue";
@@ -308,15 +133,15 @@ d3.csv("data/iris.csv").then((data) => {
   
   // brush function
   FRAME2
-  .call( d3.brush()                
-  .extent([[PADDING,PADDING], 
-    [FRAME_WIDTH, FRAME_HEIGHT - PADDING]]) 
+  .call(d3.brush()
+  .extent([[PADDING, PADDING],
+    [FRAME_WIDTH, FRAME_HEIGHT - PADDING]])
     .on("start brush", updateChart));
     
     // updates brushed points
     function updateChart(event) {
       extent = event.selection
-      points.classed("selected", function(d) { return isBrushed(extent, SEPAL_LENGTH_SCALE(d.Sepal_Length), PETAL_LENGTH_SCALE(d.Petal_Length) ); } )
+      points.classed("selected", function (d) { return isBrushed(extent, SEPAL_LENGTH_SCALE(d.Sepal_Length), PETAL_LENGTH_SCALE(d.Petal_Length)); })
       
       let selectedSpecies = [];
       
@@ -329,80 +154,84 @@ d3.csv("data/iris.csv").then((data) => {
       // points are brushed - add selected points to the selectedSpecies list
       else {
         isSelected = isBrushed(extent,
-          function (d) {SEPAL_WIDTH_SCALE(d.Sepal_Width), 
-            PETAL_WIDTH_SCALE(d.Petal_Width)});
-            if (isSelected) {
-              selectedSpecies.add(d.Species);
-            }
-            return isSelected}
-            
-            //adds selected class to bar and first frame
-            bar.classed("selected", (d) => {return selectedSpecies.has(d.Species);})
-            frame1Points.classed("selected", (d) => {return selectedSpecies.has(function (d) {
-              return d;
-            });})
+          function (d) {
+            SEPAL_WIDTH_SCALE(d.Sepal_Width),
+            PETAL_WIDTH_SCALE(d.Petal_Width)
+          });
+          if (isSelected) {
+            selectedSpecies.add(d.Species);
           }
-          
-          // confirms whether a point is brushed or not
-          function isBrushed(brush_coords, cx, cy) {
-            var x0 = brush_coords[0][0],
-            x1 = brush_coords[1][0],
-            y0 = brush_coords[0][1],
-            y1 = brush_coords[1][1];
-            return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1  ;  // This return TRUE or FALSE depending on if the points is in the selected area
-          }
-          
-          // set up frame 3
-          const FRAME3 = d3.select("#column3")
-          .append("svg")
-          .attr("height", FRAME_HEIGHT)
-          .attr("width", FRAME_WIDTH)
-          .attr("class", "frame");
-          
-          // axis labels
-          const FRAME3_X_AXIS = ["Setosa", "Versicolor", "Virginica"]
-          
-          // set up scale and x axis for graph 3
-          const X_AXIS = d3.scaleBand()
-          .range([0, VIS_WIDTH])
-          .domain(FRAME3_X_AXIS.map(function (d) { return d }))
-          .padding(0.5);
-          
-          FRAME3.append("g")
-          .attr("transform", "translate(" + PADDING + ","
-          + (VIS_HEIGHT + MARGINS.top) + ")")
-          .call(d3.axisBottom(X_AXIS));
-          
-          // set up scale and y axis for graph 3
-          const Y_AXIS = d3.scaleLinear()
-          .domain([0, 50])
-          .range([VIS_HEIGHT, 0]);
-          
-          FRAME3.append("g")
-          .attr("transform", "translate(" + PADDING + ","
-          + MARGINS.top + ")")
-          .call(d3.axisLeft(Y_AXIS).ticks(12))
-          .attr("font-size", "8px");
-          
-          // object for the data to be used in the bar
-          
-          const barData = [{Species: "virginica", Location: 87.5}, 
-          {Species: "versicolor", Location: 187.5}, 
-          {Species: "setosa", Location: 287.5}];
-          
-          // plot the bar data 
-          let bar = FRAME3.selectAll("bar")
-          .data(barData)
-          .enter()
-          .append("rect")
-          .attr("x", function (d) {return d.Location;})
-          .attr("y", 50)
-          .attr("height", VIS_HEIGHT)
-          .attr("width", 75)
-          .attr("class", function (d) {return d.Species;});
-          
+          return isSelected
+        }
+        
+        //adds selected class to bar and first frame
+        bar.classed("selected", (d) => { return selectedSpecies.has(d.Species); })
+        frame1Points.classed("selected", (d) => {
+          return selectedSpecies.has(function (d) {
+            return d;
+          });
         })
-        
-        
-        
->>>>>>> 1302d8fec16b7947576c59b12b5c273fcca016f0
+      }
+      
+      // confirms whether a point is brushed or not
+      function isBrushed(brush_coords, cx, cy) {
+        var x0 = brush_coords[0][0],
+        x1 = brush_coords[1][0],
+        y0 = brush_coords[0][1],
+        y1 = brush_coords[1][1];
+        return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;  // This return TRUE or FALSE depending on if the points is in the selected area
+      }
+      
+      // set up frame 3
+      const FRAME3 = d3.select("#column3")
+      .append("svg")
+      .attr("height", FRAME_HEIGHT)
+      .attr("width", FRAME_WIDTH)
+      .attr("class", "frame");
+      
+      // axis labels
+      const FRAME3_X_AXIS = ["Setosa", "Versicolor", "Virginica"]
+      
+      // set up scale and x axis for graph 3
+      const X_AXIS = d3.scaleBand()
+      .range([0, VIS_WIDTH])
+      .domain(FRAME3_X_AXIS.map(function (d) { return d }))
+      .padding(0.5);
+      
+      FRAME3.append("g")
+      .attr("transform", "translate(" + PADDING + ","
+      + (VIS_HEIGHT + MARGINS.top) + ")")
+      .call(d3.axisBottom(X_AXIS));
+      
+      // set up scale and y axis for graph 3
+      const Y_AXIS = d3.scaleLinear()
+      .domain([0, 50])
+      .range([VIS_HEIGHT, 0]);
+      
+      FRAME3.append("g")
+      .attr("transform", "translate(" + PADDING + ","
+      + MARGINS.top + ")")
+      .call(d3.axisLeft(Y_AXIS).ticks(12))
+      .attr("font-size", "8px");
+      
+      // object for the data to be used in the bar
+      
+      const barData = [{ Species: "virginica", Location: 87.5 },
+      { Species: "versicolor", Location: 187.5 },
+      { Species: "setosa", Location: 287.5 }];
+      
+      // plot the bar data 
+      let bar = FRAME3.selectAll("bar")
+      .data(barData)
+      .enter()
+      .append("rect")
+      .attr("x", function (d) { return d.Location; })
+      .attr("y", 50)
+      .attr("height", VIS_HEIGHT)
+      .attr("width", 75)
+      .attr("class", function (d) { return d.Species; });
+      
+    })
+    
+    
+    
